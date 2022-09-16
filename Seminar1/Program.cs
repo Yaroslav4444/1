65573f518fc643a85990d5933c10c4b47a157786
Yaroslav <perec-polosatb1i@list.ru>
2022-09-16 22:13:27 +0400

    задача 1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (a < b) max = b;
+
+Console.Write(max + " Максимальное число ");
