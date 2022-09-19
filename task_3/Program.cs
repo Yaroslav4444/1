7b2bfbe3ebe93cdcd2ccf1ae14c60cbb14050a4f
Yaroslav <perec-polosatb1i@list.ru>
2022-09-19 20:15:42 +0400

    a task 3

---
+++ task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+if (count % 2 == 0)
+{
+    Console.WriteLine("Число делится на 2");
+}
+else
+{
+    Console.WriteLine("Число не делится на 2");
+}
