5fda37d1ba5014fd0ef70cf017ef5764f09379bf
Yaroslav <perec-polosatb1i@list.ru>
2022-09-19 19:46:55 +0400

    a task 1.1

---
+++ task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число ");
+
+int userNumberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+
+int userNumberB = Convert.ToInt32(Console.ReadLine());
+int max = userNumberA;
+int min = userNumberB;
+
+if (userNumberA < userNumberB)
+{
+    max = userNumberB; min = userNumberA;
+}
+Console.WriteLine($" {max} большее, {min} меньшее ");
