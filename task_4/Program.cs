6164b93bcaeb09a432b245042725964cc061bc49
Yaroslav <perec-polosatb1i@list.ru>
2022-09-19 20:33:02 +0400

    a task 4

---
+++ task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число ");
+int count = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+Console.WriteLine("Четные числа от 1 до " + count);
+while (i <= count)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + " ");
+    }
+    i++;
+if (count == 0 | count == 1 | count < 0)
+
+Console.Write("Нет четных чисел");
+}
