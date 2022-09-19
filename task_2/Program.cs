1a2e18354bb632974bd132a23e0daaf3a6abe553
Yaroslav <perec-polosatb1i@list.ru>
2022-09-19 20:06:45 +0400

    a task 2

---
+++ task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число ");
+int userNumberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int userNumberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число ");
+int userNumberC = Convert.ToInt32(Console.ReadLine());
+int max = userNumberA;
+
+
+if (userNumberA < userNumberB)
+    {
+        max = userNumberB;
+    }
+        if (userNumberB < userNumberC)
+        {
+            max = userNumberC;
+        }
+Console.WriteLine($" {max} максимальное число");
